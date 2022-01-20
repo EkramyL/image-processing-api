8d474bd98823eb425e306ea27caeed25da6bee6b
Ekramy Latief <ekramyl3000@gmail.com>
2022-01-20 13:56:17 +0200

    fix: Errors about readdir and resolve and sharp

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,5 @@ app.use("/api", routes);
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
 });
+
+// http://localhost:3000/api/images/?filename=encenadaport&width=600&height=400
