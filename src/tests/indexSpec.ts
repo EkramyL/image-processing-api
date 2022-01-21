c6c0a5e0d5cae405a65708631262b524da8ca601
Ekramy Latief <ekramyl3000@gmail.com>
2022-01-21 19:23:42 +0200

    feat: Adding resizedimage test and fix errors

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,6 +1,8 @@
 import supertest from "supertest";
 import app from "../index";
-
+import resizeImage from "../utility/resizeImage";
+import fs from "fs";
+import path from "path";
 const request = supertest(app);
 
 describe("Test endpoint responses", () => {
@@ -9,4 +11,17 @@ describe("Test endpoint responses", () => {
     expect(res.status).toBe(200);
     done();
   });
+});
+
+describe("Testing the resizeimage function", async () => {
+  await resizeImage("encenadaport", 500, 500);
+});
+
+it("an image by the name of encenadaport_500_500.jpg should be in image", () => {
+  const imageThumbPath = path.resolve(
+    __dirname,
+    `../../imagesThumb/encenadaport_500_500.jpg`
+  );
+  const resizedImgExists = fs.existsSync(imageThumbPath);
+  expect(resizedImgExists).toBeTrue();
 });
